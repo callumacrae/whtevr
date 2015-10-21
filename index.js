da0a17ebd1489d8baf7afe443612450d7108ad34
Callum Macrae <callum@macr.ae>
2015-10-21 10:36:29 +0100

    Rename $tmpElement to $placeholder and find it instead of passing it

--- index.js
+++ index.js
@@ -13,11 +13,11 @@
    * content from the temporary <div> element.
    *
    * @param {jQuery} $scriptTag The whtevr element housing the hidden content
-   * @param {jQuery} $tmpElement The <div> tag created for the bounding rect
+   * @param {jQuery} $placeholder The <div> tag created for the bounding rect
    */
-  function removeScriptTag($scriptTag, $tmpElement) {
+  function removeScriptTag($scriptTag, $placeholder) {
     $scriptTag.remove();
-    $tmpElement.children().unwrap();
+    $placeholder.children().unwrap();
   }
 
   /**
@@ -28,27 +28,27 @@
    * when all images have been loaded.
    *
    * @param {jQuery} $scriptTag The whtevr element housing the hidden content
-   * @param {jQuery} $tmpElement The <div> tag created for the bounding rect
    */
-  function loadNow($scriptTag, $tmpElement) {
+  function loadNow($scriptTag) {
+    var $placeholder = $scriptTag.next('.whtevr-helper');
     var isNoscript = ($scriptTag.prop('tagName') === 'NOSCRIPT');
     var $content = isNoscript ? $scriptTag.text() : $scriptTag.html();
-    $tmpElement.html($.parseHTML($content));
+    $placeholder.html($.parseHTML($content));
 
     // @todo: Isn't this just the load event?
-    var $images = $tmpElement.find('img');
+    var $images = $placeholder.find('img');
     var imageTicker = 0;
     if (imageCount > 0) {
       $images.load(function () {
         ++imageTicker;
         if (imageTicker === $images.length) {
-          $scriptTag.trigger('whtevr-images-loaded', [$tmpElement]);
-          removeScriptTag($scriptTag, $tmpElement);
+          $scriptTag.trigger('whtevr-images-loaded', [$placeholder]);
+          removeScriptTag($scriptTag, $placeholder);
         }
       });
     }
 
-    $scriptTag.trigger('whtevr-loaded', [ $tmpElement ]);
+    $scriptTag.trigger('whtevr-loaded', [ $placeholder ]);
 
     // We add an additional parameter to see whether we should remove the DOM
     // element in the triggerFinished function, as we don't want to remove the
@@ -56,26 +56,26 @@
     // may not have an element to fire on if it's been removed here.
     var shouldRemove = imageCount === 0;
     if (shouldRemove) {
-      removeScriptTag($scriptTag, $tmpElement);
+      removeScriptTag($scriptTag, $placeholder);
     }
   }
 
   $('[type="text/x-whtevr"], .js-whtevr').each(function () {
     var $this = $(this);
     // We create this now because script tags don't have a bounding rect
-    var $newElement = $('<div />');
-    $newElement.insertAfter($this);
+    var $placeholder = $('<div class="whtevr-helper" />');
+    $placeholder.insertAfter($this);
 
     // data-load-after to load the element after an interval of time
     if ($this.data('load-after')) {
       $window.on('load', function () {
         setTimeout(function () {
-          loadNow($this, $newElement);
+          loadNow($this);
         }, $this.data('load-after'));
       });
     } else {
-      whenScroll(['within 300px of', $newElement[0]], function () {
-        loadNow($this, $newElement);
+      whenScroll(['within 300px of', $placeholder[0]], function () {
+        loadNow($this);
       }, true);
     }
   });
@@ -88,10 +88,7 @@
    */
   $.fn.whtevrLoad = function () {
     return this.each(function () {
-      var $this = $(this);
-      var $newElement = $('<div />').hide();
-      $newElement.insertAfter($this);
-      loadNow($this, $newElement);
+      loadNow($(this));
     });
   };
 
