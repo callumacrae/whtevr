a39d93080532b3f45f733a2e650e40243c48b3b8
Callum Macrae <callum@macr.ae>
2015-10-21 10:32:06 +0100

    Tidying, mostly comments and coding standards

--- index.js
+++ index.js
@@ -9,57 +9,47 @@
   var $window = $(window);
 
   /**
-   * Fire a custom event on $scriptTag, to say that the noscript content has
-   * been switched out.
-   * @param  {jQuery} $scriptTag  The <noscript>/<script> tag which houses the hidden content
-   * @param  {jQuery} $tmpElement The <div> tag which temporarily houses the loaded content
-   * @return {null}
-   */
-  function triggerFinished($scriptTag, $tmpElement) {
-    $scriptTag
-      .trigger('whtevr-loaded', [ $tmpElement ]);
-  }
-
-  /**
-   * Removes the <script>/<noscript> tag from the DOM, and unwraps the loaded
+   * Removes the hidden whtevr element from the DOM, and unwraps the loaded
    * content from the temporary <div> element.
-   * @param  {jQuery} $scriptTag  The <noscript>/<script> tag which houses the hidden content
-   * @param  {jQuery} $tmpElement The <div> tag which temporarily houses the loaded content
-   * @return {null}
+   *
+   * @param {jQuery} $scriptTag The whtevr element housing the hidden content
+   * @param {jQuery} $tmpElement The <div> tag created for the bounding rect
    */
   function removeScriptTag($scriptTag, $tmpElement) {
-    $scriptTag
-      .remove();
+    $scriptTag.remove();
     $tmpElement.children().unwrap();
   }
 
   /**
-   * Takes the content within the <noscript>/<script> tag, and outputs it to a
-   * temporary <div> element which is created to house the content for
-   * scroll-based calculations. Also checks for images, and fires an event when
-   * all images have been loaded.
-   * @param  {jQuery} $scriptTag  The <noscript>/<script> tag which houses the hidden content
-   * @param  {jQuery} $tmpElement The <div> tag which temporarily houses the loaded content
-   * @return {null}
+   * Loads the contents of the whtevr element.
+   *
+   * Takes the content within the whtevr element, and outputs it to a temporary
+   * <div> element created earlier. Also checks for images, and fires an event
+   * when all images have been loaded.
+   *
+   * @param {jQuery} $scriptTag The whtevr element housing the hidden content
+   * @param {jQuery} $tmpElement The <div> tag created for the bounding rect
    */
   function loadNow($scriptTag, $tmpElement) {
     var isNoscript = ($scriptTag.prop('tagName') === 'NOSCRIPT');
     var $content = isNoscript ? $scriptTag.text() : $scriptTag.html();
     $tmpElement.html($.parseHTML($content));
+
+    // @todo: Isn't this just the load event?
     var $images = $tmpElement.find('img');
     var imageTicker = 0;
-    var imageCount = $images.length;
     if (imageCount > 0) {
       $images.load(function () {
         ++imageTicker;
-        if (imageCount === imageTicker) {
-          $scriptTag
-            .trigger('whtevr-images-loaded', [$tmpElement]);
+        if (imageTicker === $images.length) {
+          $scriptTag.trigger('whtevr-images-loaded', [$tmpElement]);
           removeScriptTag($scriptTag, $tmpElement);
         }
       });
     }
-    triggerFinished($scriptTag, $tmpElement);
+
+    $scriptTag.trigger('whtevr-loaded', [ $tmpElement ]);
+
     // We add an additional parameter to see whether we should remove the DOM
     // element in the triggerFinished function, as we don't want to remove the
     // element if we have images to load, as the `whtevr-images-loaded` trigger
@@ -76,6 +66,7 @@
     var $newElement = $('<div />');
     $newElement.insertAfter($this);
 
+    // data-load-after to load the element after an interval of time
     if ($this.data('load-after')) {
       $window.on('load', function () {
         setTimeout(function () {
@@ -86,27 +77,19 @@
       whenScroll(['within 300px of', $newElement[0]], function () {
         loadNow($this, $newElement);
       }, true);
-
     }
   });
 
   /**
    * Enables calling .whtevrLoad() on an element to load the content within that
    * tag. The tag should be either a <noscript> or <script> tag.
-   * @return {function} Loop through each of the matched elements
+   *
+   * @return {jQuery} Self.
    */
   $.fn.whtevrLoad = function () {
-    /**
-     * Sets up the plugin by running the loadNow function, after creating an
-     * empty <div> tag to house the content temporarily.
-     * @param  {int} idx Counter
-     * @param  {HTMLElement} elem  The individual element that matches the selector given to the plugin.
-     * @return {null}
-     */
-    return this.each(function (idx, elem) {
-      var $this = $(elem);
-      var $newElement = $('<div />')
-        .hide();
+    return this.each(function () {
+      var $this = $(this);
+      var $newElement = $('<div />').hide();
       $newElement.insertAfter($this);
       loadNow($this, $newElement);
     });
