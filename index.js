bbdf1d55dc0ca4559d64dc879195026cf0d22168
Callum Macrae <callum@macr.ae>
2015-10-21 11:01:10 +0100

    es6 and spaces -> tabs

--- index.js
+++ index.js
@@ -1,96 +1,87 @@
-(function (root, factory) {
-  if (typeof exports === 'object') {
-    factory(require('jquery'), require('when-scroll'));
-  } else {
-    factory(root.$, root.whenScroll);
-  }
-}(this, function ($, whenScroll) {
+import jquery from 'jquery';
+import whenScroll from 'when-scroll';
 
-  var $window = $(window);
+var $window = $(window);
 
-  /**
-   * Removes the hidden whtevr element from the DOM, and unwraps the loaded
-   * content from the temporary <div> element.
-   *
-   * @param {jQuery} $scriptTag The whtevr element housing the hidden content
-   * @param {jQuery} $placeholder The <div> tag created for the bounding rect
-   */
-  function removeScriptTag($scriptTag, $placeholder) {
-    $scriptTag.remove();
-    $placeholder.children().unwrap();
-  }
+/**
+ * Removes the hidden whtevr element from the DOM, and unwraps the loaded
+ * content from the temporary <div> element.
+ *
+ * @param {jQuery} $scriptTag The whtevr element housing the hidden content
+ * @param {jQuery} $placeholder The <div> tag created for the bounding rect
+ */
+function removeScriptTag($scriptTag, $placeholder) {
+	$scriptTag.remove();
+	$placeholder.children().unwrap();
+}
 
-  /**
-   * Loads the contents of the whtevr element.
-   *
-   * Takes the content within the whtevr element, and outputs it to a temporary
-   * <div> element created earlier. Also checks for images, and fires an event
-   * when all images have been loaded.
-   *
-   * @param {jQuery} $scriptTag The whtevr element housing the hidden content
-   */
-  function loadNow($scriptTag) {
-    var $placeholder = $scriptTag.next('.whtevr-helper');
-    var isNoscript = ($scriptTag.prop('tagName') === 'NOSCRIPT');
-    var $content = isNoscript ? $scriptTag.text() : $scriptTag.html();
-    $placeholder.html($.parseHTML($content));
+/**
+ * Loads the contents of the whtevr element.
+ *
+ * Takes the content within the whtevr element, and outputs it to a temporary
+ * <div> element created earlier. Also checks for images, and fires an event
+ * when all images have been loaded.
+ *
+ * @param {jQuery} $scriptTag The whtevr element housing the hidden content
+ */
+function loadNow($scriptTag) {
+	var $placeholder = $scriptTag.next('.whtevr-helper');
+	var isNoscript = ($scriptTag.prop('tagName') === 'NOSCRIPT');
+	var $content = isNoscript ? $scriptTag.text() : $scriptTag.html();
+	$placeholder.html($.parseHTML($content));
 
-    // @todo: Isn't this just the load event?
-    var $images = $placeholder.find('img');
-    var imageCount = $images.length;
-    var imageTicker = 0;
-    if (imageCount > 0) {
-      $images.load(function () {
-        ++imageTicker;
-        if (imageTicker === imageCount) {
-          $scriptTag.trigger('whtevr-images-loaded', [$placeholder]);
-          removeScriptTag($scriptTag, $placeholder);
-        }
-      });
-    }
+	// @todo: Isn't this just the load event?
+	var $images = $placeholder.find('img');
+	var imageCount = $images.length;
+	var imageTicker = 0;
+	if (imageCount > 0) {
+		$images.load(function () {
+			++imageTicker;
+			if (imageTicker === imageCount) {
+				$scriptTag.trigger('whtevr-images-loaded', [$placeholder]);
+				removeScriptTag($scriptTag, $placeholder);
+			}
+		});
+	}
 
-    $scriptTag.trigger('whtevr-loaded', [ $placeholder ]);
+	$scriptTag.trigger('whtevr-loaded', [ $placeholder ]);
 
-    // We add an additional parameter to see whether we should remove the DOM
-    // element in the triggerFinished function, as we don't want to remove the
-    // element if we have images to load, as the `whtevr-images-loaded` trigger
-    // may not have an element to fire on if it's been removed here.
-    var shouldRemove = imageCount === 0;
-    if (shouldRemove) {
-      removeScriptTag($scriptTag, $placeholder);
-    }
-  }
+	// We add an additional parameter to see whether we should remove the DOM
+	// element in the triggerFinished function, as we don't want to remove the
+	// element if we have images to load, as the `whtevr-images-loaded` trigger
+	// may not have an element to fire on if it's been removed here.
+	var shouldRemove = imageCount === 0;
+	if (shouldRemove) {
+		removeScriptTag($scriptTag, $placeholder);
+	}
+}
 
-  $('[type="text/x-whtevr"], .js-whtevr').each(function () {
-    var $this = $(this);
-    // We create this now because script tags don't have a bounding rect
-    var $placeholder = $('<div class="whtevr-helper" />');
-    $placeholder.insertAfter($this);
+$('[type="text/x-whtevr"], .js-whtevr').each(function () {
+	var $this = $(this);
+	// We create this now because script tags don't have a bounding rect
+	var $placeholder = $('<div class="whtevr-helper" />');
+	$placeholder.insertAfter($this);
 
-    // data-load-after to load the element after an interval of time
-    if ($this.data('load-after')) {
-      $window.on('load', function () {
-        setTimeout(function () {
-          loadNow($this);
-        }, $this.data('load-after'));
-      });
-    } else {
-      whenScroll(['within 300px of', $placeholder[0]], function () {
-        loadNow($this);
-      }, true);
-    }
-  });
+	// data-load-after to load the element after an interval of time
+	if ($this.data('load-after')) {
+		$window.on('load', function () {
+			setTimeout(() => loadNow($this), $this.data('load-after'));
+		});
+	} else {
+		whenScroll(['within 300px of', $placeholder[0]], function () {
+			loadNow($this);
+		}, true);
+	}
+});
 
-  /**
-   * Enables calling .whtevrLoad() on an element to load the content within that
-   * tag. The tag should be either a <noscript> or <script> tag.
-   *
-   * @return {jQuery} Self.
-   */
-  $.fn.whtevrLoad = function () {
-    return this.each(function () {
-      loadNow($(this));
-    });
-  };
-
-}));
+/**
+ * Enables calling .whtevrLoad() on an element to load the content within that
+ * tag. The tag should be either a <noscript> or <script> tag.
+ *
+ * @return {jQuery} Self.
+ */
+$.fn.whtevrLoad = function () {
+	return this.each(function () {
+		loadNow($(this));
+	});
+};
--- test/whtevr.spec.js
+++ test/whtevr.spec.js
@@ -2,150 +2,150 @@
 'use strict';
 
 describe('whtevr', function () {
-  var immediateLoadEvent = false;
-  $('.js-load-immediately').on('whtevr-loaded', function () {
-    immediateLoadEvent = true;
-  });
+	var immediateLoadEvent = false;
+	$('.js-load-immediately').on('whtevr-loaded', function () {
+		immediateLoadEvent = true;
+	});
 
-  after(function () {
-    $(window).scrollTop(0);
-    $('.deletewhendone').remove();
-  });
+	after(function () {
+		$(window).scrollTop(0);
+		$('.deletewhendone').remove();
+	});
 
-  describe('non-lazy loading', function () {
-    it('should load immediately if on screen', function () {
-      $(document).on('ready', function () {
-        $('#test3').length.should.equal(1);
-      });
+	describe('non-lazy loading', function () {
+		it('should load immediately if on screen', function () {
+			$(document).on('ready', function () {
+				$('#test3').length.should.equal(1);
+			});
 
-    });
+		});
 
-    it('should fire an event immediately if on screen', function () {
-      $(document).on('ready', function () {
-        immediateLoadEvent.should.equal(true);
-      });
-    });
-  });
+		it('should fire an event immediately if on screen', function () {
+			$(document).on('ready', function () {
+				immediateLoadEvent.should.equal(true);
+			});
+		});
+	});
 
-  describe('lazy load on delay', function () {
-    it('should not load stuff early', function () {
-      $('#test2').length.should.equal(0);
-    });
+	describe('lazy load on delay', function () {
+		it('should not load stuff early', function () {
+			$('#test2').length.should.equal(0);
+		});
 
-    it('should load stuff after a bit tho', function (done) {
-      $(document).ready(function () {
-        setTimeout(function () {
-          $('#test2').length.should.equal(1);
-          done();
-        }, 150);
-      });
-    });
-  });
+		it('should load stuff after a bit tho', function (done) {
+			$(document).ready(function () {
+				setTimeout(function () {
+					$('#test2').length.should.equal(1);
+					done();
+				}, 150);
+			});
+		});
+	});
 
-  describe('lazy load on scroll', function () {
-    var eventFired = false;
-    var $element;
+	describe('lazy load on scroll', function () {
+		var eventFired = false;
+		var $element;
 
-    before(function () {
-      $('[type="text/x-whtevr"]').on('whtevr-loaded', function (e, $el) {
-        eventFired = true;
-        $element = $el;
-      });
-      $('[type="text/x-whtevr-experiment"]').whtevrLoad();
-    });
+		before(function () {
+			$('[type="text/x-whtevr"]').on('whtevr-loaded', function (e, $el) {
+				eventFired = true;
+				$element = $el;
+			});
+			$('[type="text/x-whtevr-experiment"]').whtevrLoad();
+		});
 
-    it('should not load stuff early', function () {
-      $('#test').length.should.equal(0);
-    });
+		it('should not load stuff early', function () {
+			$('#test').length.should.equal(0);
+		});
 
-    it('should load stuff', function (done) {
-      setTimeout(function () {
-        $(window).scrollTop(1700);
-      });
+		it('should load stuff', function (done) {
+			setTimeout(function () {
+				$(window).scrollTop(1700);
+			});
 
-      var interval = setInterval(function () {
-        if ($('#test').length === 1) {
-          done();
-          clearInterval(interval);
-        }
-      }, 10);
-    });
+			var interval = setInterval(function () {
+				if ($('#test').length === 1) {
+					done();
+					clearInterval(interval);
+				}
+			}, 10);
+		});
 
-    it('should show content on event', function () {
-      $('#test-fn-1').length.should.equal(1);
-    });
+		it('should show content on event', function () {
+			$('#test-fn-1').length.should.equal(1);
+		});
 
-    it('should have fired an event', function () {
-      eventFired.should.be.True;
-      $element.length.should.equal(1);
-      //Commenting this out because I'm removing the wrapping <div> when
-      //$element.is(':visible').should.be.True;
-    });
-  });
+		it('should have fired an event', function () {
+			eventFired.should.be.True;
+			$element.length.should.equal(1);
+			//Commenting this out because I'm removing the wrapping <div> when
+			//$element.is(':visible').should.be.True;
+		});
+	});
 
-  describe('noscript', function () {
-    var eventFired = false;
-    var $element;
-    var imageHasLoaded = false;
-    var brokenImageHasLoaded = false;
+	describe('noscript', function () {
+		var eventFired = false;
+		var $element;
+		var imageHasLoaded = false;
+		var brokenImageHasLoaded = false;
 
-    before(function () {
-      $('.js-whtevr').on('whtevr-loaded', function (e, $el) {
-        eventFired = true;
-        $element = $el;
-      });
+		before(function () {
+			$('.js-whtevr').on('whtevr-loaded', function (e, $el) {
+				eventFired = true;
+				$element = $el;
+			});
 
-      $('.js-image').whtevrLoad();
+			$('.js-image').whtevrLoad();
 
-      $('.js-image').on('whtevr-images-loaded', function (e, $el) {
-        imageHasLoaded = true;
+			$('.js-image').on('whtevr-images-loaded', function (e, $el) {
+				imageHasLoaded = true;
 
-      });
+			});
 
-      $('.js-broken-image').on('whtevr-images-loaded', function (e, $el) {
-        brokenImageHasLoaded = true;
-      });
-    });
+			$('.js-broken-image').on('whtevr-images-loaded', function (e, $el) {
+				brokenImageHasLoaded = true;
+			});
+		});
 
-    it('should not load stuff early', function () {
-      $('#test4').length.should.equal(0);
-    });
+		it('should not load stuff early', function () {
+			$('#test4').length.should.equal(0);
+		});
 
-    it('should load stuff', function (done) {
-      setTimeout(function () {
-        $(window).scrollTop(8700);
-      });
+		it('should load stuff', function (done) {
+			setTimeout(function () {
+				$(window).scrollTop(8700);
+			});
 
-      var interval = setInterval(function () {
-        if ($('#test4').length === 1) {
-          done();
-          clearInterval(interval);
-        }
-      }, 10);
-    });
+			var interval = setInterval(function () {
+				if ($('#test4').length === 1) {
+					done();
+					clearInterval(interval);
+				}
+			}, 10);
+		});
 
-    it('should have loaded an image', function (done) {
-      var interval = setInterval(function () {
-        if (imageHasLoaded === true) {
-          done();
-          clearInterval(interval);
-        }
-      }, 10);
-    });
+		it('should have loaded an image', function (done) {
+			var interval = setInterval(function () {
+				if (imageHasLoaded === true) {
+					done();
+					clearInterval(interval);
+				}
+			}, 10);
+		});
 
-    it('should not load an unbroken image', function (done) {
-      setTimeout(function () {
-        if (brokenImageHasLoaded === false) {
-          done();
-        }
-      }, 250);
-    });
+		it('should not load an unbroken image', function (done) {
+			setTimeout(function () {
+				if (brokenImageHasLoaded === false) {
+					done();
+				}
+			}, 250);
+		});
 
-    it('should have fired an event', function () {
-      eventFired.should.be.True;
-      $element.length.should.equal(1);
-      // Commenting this out because I'm removing the wrapping <div> when
-      //$element.is(':visible').should.be.True;
-    });
-  });
+		it('should have fired an event', function () {
+			eventFired.should.be.True;
+			$element.length.should.equal(1);
+			// Commenting this out because I'm removing the wrapping <div> when
+			//$element.is(':visible').should.be.True;
+		});
+	});
 });
